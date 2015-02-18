1f757d04a7f4a953bbacf1d2cbea61390f71a6ae
Steven Velozo <steven@velozo.com>
2015-02-17 22:17:21 -0800

    Still testing patterns for multi-module connection

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-tmpFH = require('../foxhound');
+var tmpFH = require('../foxhound');
 
-tmpFH.begin(40);
+tmpFH.setLogLevel(3);
+
+tmpFH.setScope('Users');
+
+console.log('Scope:'+tmpFH.parameters.scope + ' DataElements:' + tmpFH.parameters.dataElements + ' Begin:'+tmpFH.parameters.begin)
+
+tmpFH.buildFetchQuery();
+
+console.log('Query: '+tmpFH.query.body)

57df3a17351c037a39d41ac51efd453d433772af
Steven Velozo <steven@velozo.com>
2015-04-10 14:31:52 -0700

    Completed schema validation

---
+++ source/Meadow-Schema.js
@@ -0,0 +1,126 @@
+/**
+* Meadow Schema Module
+*
+* @license MIT
+*
+* @author Steven Velozo <steven@velozo.com>
+* @module Meadow-Schema
+*/
+
+/**
+* Meadow Schema Library
+*
+* @class MeadowSchema
+* @constructor
+*/
+var libValidator = require('is-my-json-valid')
+
+var MeadowSchema = function()
+{
+	function createNew(pOriginalSchema)
+	{
+		/* The container object JsonSchema
+		 * http://json-schema.org/examples.html
+		 * http://json-schema.org/latest/json-schema-core.html
+		 *
+		 * An example:
+			{
+				"$schema": "http://json-schema.org/draft-04/schema#",
+				"title": "Product",
+				"description": "A product from Acme's catalog",
+				"type": "object",
+				"properties": {
+					"id": {
+						"description": "The unique identifier for a product",
+						"type": "integer"
+					},
+					"name": {
+						"description": "Name of the product",
+						"type": "string"
+					},
+					"price": {
+						"type": "number",
+						"minimum": 0,
+						"exclusiveMinimum": true
+					},
+					"tags": {
+						"type": "array",
+						"items": {
+							"type": "string"
+						},
+						"minItems": 1,
+						"uniqueItems": true
+					}
+				},
+				"required": ["id", "name", "price"]
+			}
+		*/
+		var _Schema = false;
+		var _Validate = false;
+
+
+		/**
+		* Set the schema to be something else after the object is created.
+		*
+		* @method setSchema
+		*/
+		var setSchema = function(pSchema)
+		{
+			_Schema = (typeof(pSchema) === 'object') ? pSchema : (
+			{
+				title: 'Unknown',
+				type: 'object',
+				required: []
+			});
+			_Validate = libValidator(_Schema, { greedy:true, verbose:true })
+		};
+		setSchema(pOriginalSchema);
+
+
+		/**
+		* Validate an object against the current schema
+		*
+		* @method setSchema
+		*/
+		var validateObject = function(pObject)
+		{
+			var tmpValidation = { Valid:_Validate(pObject) };
+
+			// Stuff the errors in if it is invalid
+			if (!tmpValidation.Valid)
+			{
+				tmpValidation.Errors = _Validate.errors;
+			}
+
+			return tmpValidation;
+		};
+
+
+		var tmpNewMeadowSchemaObject = (
+		{
+			setSchema: setSchema,
+			validateObject: validateObject,
+
+			new: createNew
+		});
+
+		/**
+		 * Schema
+		 *
+		 * @property schema
+		 * @type object
+		 */
+		Object.defineProperty(tmpNewMeadowSchemaObject, 'schema',
+			{
+				get: function() { return _Schema; },
+				enumerable: true
+			});
+
+
+		return tmpNewMeadowSchemaObject;
+	}
+
+	return createNew();
+};
+
+module.exports = MeadowSchema();
--- test/Meadow_tests.js
+++ test/Meadow_tests.js
@@ -10,18 +10,53 @@ var Chai = require("chai");
 var Expect = Chai.expect;
 var Assert = Chai.assert;
 
+var libFable = require('fable');
+
+var _AnimalSchema = (
+{
+	"title": "Animal",
+	"description": "A creature that lives in a meadow.",
+	"type": "object",
+	"properties": {
+		"id": {
+			"description": "The unique identifier for an animal",
+			"type": "integer"
+		},
+		"type": {
+			"description": "The type of the animal",
+			"type": "string"
+		},
+		"name": {
+			"description": "The animal's name",
+			"type": "string"
+		},
+		"age": {
+			"description": "How old the animal is in days",
+			"type": "number",
+			"minimum": 0,
+			"exclusiveMinimum": true
+		},
+		"tags": {
+			"type": "array",
+			"items": {
+				"type": "string"
+			},
+			"minItems": 0,
+			"uniqueItems": true
+		}
+	},
+	"required": ["id", "name", "age"]
+});
+
 suite
 (
 	'Meadow',
 	function()
 	{
-		var testMeadow = false;
-
 		setup
 		(
 			function()
 			{
-				testMeadow = require('../source/Meadow.js');
 			}
 		);
 
@@ -35,20 +70,71 @@ suite
 					'The class should initialize itself into a happy little object.',
 					function()
 					{
+						var testMeadow = require('../source/Meadow.js').new();
 						Expect(testMeadow).to.be.an('object', 'Meadow should initialize as an object directly from the require statement.');
 					}
 				);
 				test
 				(
-					'There should be some basic metadata on the class parameters',
+					'There should be some basic metadata on the class properties',
 					function()
 					{
+						var testMeadow = require('../source/Meadow.js').new(libFable);
 						Expect(testMeadow).to.have.a.property('scope')
 						.that.is.a('string'); // Scope is always a string
-
-						Expect(testMeadow).to.have.a.property('uuid')
-						.that.is.a('string')
-						.that.is.not.empty;
+						Expect(testMeadow).to.have.a.property('schema')
+						.that.is.a('object');
+					}
+				);
+				test
+				(
+					'Initialize with values',
+					function()
+					{
+						var testMeadow = require('../source/Meadow.js').new(libFable, 'Animal', _AnimalSchema);
+						Expect(testMeadow.scope)
+							.to.equal('Animal');
+						Expect(testMeadow.schema.title)
+							.to.equal('Animal');
+					}
+				);
+				test
+				(
+					'Alternative initialization',
+					function()
+					{
+						var testMeadow = require('../source/Meadow.js').new(libFable)
+							.setScope('Animal')
+							.setSchema(_AnimalSchema);
+						Expect(testMeadow.scope)
+							.to.equal('Animal');
+						var tmpValidationResults = testMeadow.validateObject({id:10, type:'bunny', name:'foofoo', age:3});
+						Expect(tmpValidationResults.Valid)
+							.to.equal(true);
+					}
+				);
+				test
+				(
+					'Validate a proper animal',
+					function()
+					{
+						var testMeadow = require('../source/Meadow.js').new(libFable, 'Animal', _AnimalSchema);
+						var tmpValidationResults = testMeadow.validateObject({id:10, type:'bunny', name:'foofoo', age:3});
+						Expect(tmpValidationResults.Valid)
+							.to.equal(true);
+					}
+				);
+				test
+				(
+					'Validate a messed up animal',
+					function()
+					{
+						var testMeadow = require('../source/Meadow.js').new(libFable, 'Animal', _AnimalSchema);
+						// Our zombie needs a name!
+						var tmpValidationResults = testMeadow.validateObject({id:9, type:'zombie', age:3});
+						libFable.log.info('Bad Unnamed Zombie Validation Results', tmpValidationResults);
+						Expect(tmpValidationResults.Valid)
+							.to.equal(false);
 					}
 				);
 			}
